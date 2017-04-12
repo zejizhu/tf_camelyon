cc1103baa207d5bc4ef9b2c73a46d4d8aa1f6949
zeki <zhuzeji@126.com>
2017-04-12 10:43:30 +0800

    change the top python script

--- config.py
+++ config.py
@@ -1,40 +1,80 @@
 import os
 import sys
 
+class conifg_run():
+    def __init__(self):
+        self.mode="test"
+
 class config_path:
     def __init__(self):
         self.data_dir_top="/home1/zhuzj/dataset/camelyon16_B2/"
-        self.train_dir="raw-data/train"
-        self.data_dir="TFRecords/"
-        self.eva_dir="raw-data/validation"
-        self.model_checkpoint_path="model"
+        self.data_dir = "TFRecords/"
+        self.train_dir="events/train/"
+        self.eva_dir="events/eval/"
+        self.test_dir="events/test/"
+        self.csv_dir="csv_outs"
+        self.mode_dir="models"
+        self.model_checkpoint="model.ckpt-04082045-80000"
         self.top_dir= sys.path[0]
         self.inception_dir ="inception"
 
 class config_param:
     def __init__(self):
-        self.init_learning_rate=0.001
+        self.init_learning_rate=0.01
         self.input_queue_memory_factor=1
-        self.max_step = 10000
-        self.gpu_cnt = 2
+        self.max_step = 160000
+        self.gpu_num = 2
+        self.batch_size=2
         self.fine_tune =1
+        self.eval_num_example=8196
+        self.test_num_example=2370610
 
+class config_const:
+    def __init__(self):
+        self.TRAIN_MODE="train"
+        self.EVAL_MODE="eval"
+        self.TEST_MODE="test"
 
-
-
-
-
-def config_init():
+def dir_check():
     path = config_path()
-    DATA_DIR_TOP = path.data_dir_top
-    if os.path.exists(DATA_DIR_TOP):
-        print("%s is exists!" %(DATA_DIR_TOP))
+    train_dir=os.path.join(path.data_dir_top,path.data_dir)
+    if os.path.exists(train_dir):
+        print("%s is exists!" %(train_dir))
     else:
-        print("%s is not exists !" %(DATA_DIR_TOP))
+        os.makedirs(train_dir)
+        print("creat the dit %s! "%(train_dir))
+    eval_dir = os.path.join(path.data_dir_top,path.eva_dir)
+    if os.path.exists(eval_dir):
+        print("%s is exists!" %(eval_dir))
+    else:
+        os.makedirs(eval_dir)
+        print("creat the dit %s! "%(eval_dir))
+
+    test_dir = os.path.join(path.data_dir_top,path.test_dir)
+    if os.path.exists(test_dir):
+        print("%s is exists!" %(test_dir))
+    else:
+        os.makedirs(eval_dir)
+        print("creat the dit %s! "%(test_dir))
+
+    return 0
+def mode_check():
+    run = conifg_run()
+    const_val = config_const()
+    if run.mode == const_val.TEST_MODE:
+        print("the run mode is %s!" %(run.mode ))
+    elif run.mode == const_val.TRAIN_MODE:
+        print("the run mode is %s!" % (run.mode))
+    elif run.mode == const_val.EVAL_MODE:
+        print("the run mode is %s!" % (run.mode))
+    else:
+        print("Input mode [%s] fail! "%(run.mode))
+        return  1
+    return  0
+
+def init():
+    if mode_check()!=0:
         return 1
-    tmp_path = os.path.join(DATA_DIR_TOP,path.model_checkpoint_path)
-    if os.path.exists(tmp_path):
-        print("%s is exists!" % (tmp_path))
-    else:
-        os.makedirs(tmp_path)
+    if dir_check() !=0:
+        return 1
     return  0
--- demo.py
+++ demo.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import config as cfg
+import numpy as np
 path = cfg.config_path()
 sys.path.append(path.top_dir)
 import inception
@@ -13,28 +14,25 @@ def train_init():
 
 def train():
     train_init()
-    path = cfg.config_path()
-    param = cfg.config_param()
-    build_tf ="bazel build inception/camelyon_train"
-    print(build_tf)
-    os.system(build_tf)
-    TRAIN_DIR =os.path.join(path.data_dir_top,path.train_dir)
-    DATA_DIR=os.path.join(path.data_dir_top,path.data_dir)
-    EVA_DIR=os.path.join(path.data_dir_top,path.eva_dir)
-    run_train="bazel-bin/inception/camelyon_train --train_dir=%s --data_dir=%s --eval_dir=%s --fine_tune=%s " \
-              "--initial_learning_rate=%lf --input_queue_memory_factor=%d" \
-              %(TRAIN_DIR,DATA_DIR,EVA_DIR,param.fine_tune,param.init_learning_rate,param.input_queue_memory_factor)
-    print(run_train)
-    os.system(run_train)
 
-def eval():
+
+def eval_init():
     return 0
 
+def eval(a):
+    b= np.log()
+    return 0
+
+def test_init():
+    return  0
+def test():
+    return 0
 
 def main():
-    print("train next!")
-    train()
-
+    cfg_run = cfg.conifg_run()
+    if cfg.init() != 0:
+        print("config init fail!")
+        return 1
 
 
 
